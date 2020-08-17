5cce18add41008b6487a9035fa3236d7cedcc57a
Shaidee Alingcastre <shaidee@alingcastre.net>
2020-08-17 14:32:09 -0700

    added instruction

--- Recursion/UdemyDataAlgCourse/ProductOfArray/index.js
+++ Recursion/UdemyDataAlgCourse/ProductOfArray/index.js
@@ -1,8 +1,12 @@
+// Write a function called productOfArray which takes ina an array of numbers and returns the product of them all.
 
-
-function productOfArray(arr){
-    if(arr.length ===1){
+function productOfArray(arr) {
+    if (arr.length === 1) {
         return arr[0];
     }
+    
     return arr[0] * productOfArray(arr.slice(1));
 }
+
+console.log(productOfArray([1,2,3]))     // 6
+console.log(productOfArray([1,2,3,10]))  // 60
