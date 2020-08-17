37a05b253de7c2bf7b82a2d9f311639128354dff
Shaidee Alingcastre <shaidee@alingcastre.net>
2020-08-17 14:19:41 -0700

    added solution

---
+++ Recursion/UdemyDataAlgCourse/Flatten/index.js
@@ -0,0 +1,20 @@
+// Write a recursive function called flatten which accepts an array of arrays and returnrs a new array with all  values flattened.
+
+function flatten(oldArr){
+  var newArr = [];
+  
+  for(var i = 0; i < oldArr.length; i++){
+    if(Array.isArray(oldArr[i])){
+        newArr = newArr.concat(flatten(oldArr[i]))
+    } else {
+        newArr.push(oldArr[i])
+    }
+  } 
+  
+  return newArr;
+}
+
+console.log(flatten([1, 2, 3, [4, 5] ]))                  // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]]))             // [1, 2, 3, 4, 5]
+console.log(flatten([[1],[2],[3]]))                       // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3
