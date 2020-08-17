7177df47dfda89462d503d586c0ec011e17edee7
Shaidee Alingcastre <shaidee@alingcastre.net>
2020-08-17 14:40:01 -0700

    added instruction

--- Recursion/UdemyDataAlgCourse/Reverse/index.js
+++ Recursion/UdemyDataAlgCourse/Reverse/index.js
@@ -1,8 +1,12 @@
+// Write a recursive function called reverse which accepts a string and returns a new string in reverse.
 
-
-function reverse(str){
-    if ( str.length ===1){
-        return str[0]
+function reverse(str) {
+    if (str.length === 1) {
+        return str[0];
     }
-    return str[str.length-1] + reverse(str.subString(0,str.length-1))
+    
+    return str[str.length - 1] + reverse(str.substring(0, str.length - 1));
 }
+
+console.log(reverse('awesome'));     // 'emosewa'
+console.log(reverse('rithmschool')); // 'loohcsmhtir'
