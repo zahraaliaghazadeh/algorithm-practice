709101046eeede01f04b6d6bd63c069ec4662a56
Shaidee Alingcastre <shaidee@alingcastre.net>
2020-08-17 14:43:09 -0700

    added instruction

--- Recursion/UdemyDataAlgCourse/isPalindrome/index.js
+++ Recursion/UdemyDataAlgCourse/isPalindrome/index.js
@@ -1,10 +1,22 @@
-
+/*
+    Write a recursive function called isPalindrome which returns true if the string passed to it 
+    is a palindrome (reads the same forward and backward). Otherwise it returns false.
+*/
 
 function isPalindrome(str){
-    if (str.length===1) return true
-    if (str.length ===2) return str[0]===str[1]
-    if(str[0]===str[str.length-1]){
-        return isPalindrome(str.subString(1,str.length-1));
+    if (str.length === 1) return true;
+    
+    if (str.length === 2) return str[0] === str[1];
+    
+    if (str[0] === str[str.length - 1]) {
+        return isPalindrome(str.subString(1, str.length - 1));
     }
-    return false
+    
+    return false;
 }
+
+console.log(isPalindrome('awesome'));                    // false
+console.log(isPalindrome('foobar'));                     // false
+console.log(isPalindrome('tacocat'));                    // true
+console.log(isPalindrome('amanaplanacanalpanama'));      // true
+console.log(isPalindrome('amanaplanacanalpandemonium')); // false
