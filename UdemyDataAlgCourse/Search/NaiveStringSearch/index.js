9a09f2648117a72012d06e853f54ff50f0f18a1e
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-08-18 14:46:35 -0700

    naive string search

--- UdemyDataAlgCourse/Search/NaiveStringSearch/index.js
+++ UdemyDataAlgCourse/Search/NaiveStringSearch/index.js
@@ -1,1 +1,19 @@
 // to count the number of times a string appeared in a string
+// O(n^2)
+
+
+// this function might need to be cleaned up
+function naiveSearch(long,short){
+var count = 0;
+for(var i= 0 ; i< short.length ; i++){
+  for(var j= 0 ; j< short.length ; j++){
+   if (short[j] !== long[i+j]) {
+       break;
+   }
+   if(j === short.length-1){
+       count ++
+   }
+}
+}
+return count
+}
